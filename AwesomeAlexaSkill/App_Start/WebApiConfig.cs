71da571ebb54fdf3270af3f0fa022a0456601b74
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2019-09-04 21:54:01 -0300

    Add identity logic configuration logicc in order to work with OAuth

--- AwesomeAlexaSkill/App_Start/WebApiConfig.cs
+++ AwesomeAlexaSkill/App_Start/WebApiConfig.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 using AwesomeAlexaSkill.Handlers;
+using Microsoft.Owin.Security.OAuth;
 
 namespace AwesomeAlexaSkill
 {
@@ -11,6 +9,9 @@ namespace AwesomeAlexaSkill
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ AwesomeAlexaSkill/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AwesomeAlexaSkill.Startup))]
+namespace AwesomeAlexaSkill
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
